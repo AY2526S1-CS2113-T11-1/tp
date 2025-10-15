31a8a4c4f518d8179804b9f831b573198883336d
k1b1t0 <nguyenminhduc2882005@gmail.com>
2025-10-15 22:48:54 +0800

    Fix: Checkstyle for test and main

--- src/test/java/quizmos/command/ReviewCommandTest.java
+++ src/test/java/quizmos/command/ReviewCommandTest.java
@@ -1,12 +1,8 @@
 package quizmos.command;
 
 import org.junit.jupiter.api.Test;
-import quizmos.flashcard.Flashcard;
-import quizmos.flashcardlist.FlashcardList;
-import quizmos.storage.Storage;
-import quizmos.ui.Ui;
 
-import java.io.*;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
